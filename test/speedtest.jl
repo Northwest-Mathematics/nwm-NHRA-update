d90cc096a2a0e97fa6c1a23eb7905e5d29c622ee
zach-nwm <zach.gelbaum@northwestmath.com>
2024-05-12 11:01:48 -0700

    naive phi^-, unoptimized,155 sec Nx=400000, Nj=30

--- test/speedtest.jl
+++ test/speedtest.jl
@@ -1,11 +1,19 @@
-# Oregon is about 254806 km², so .5km² cells gives
 
-Nx = 4 * (254806) # number of alternatives
-Nj = 30 # number of criteria
+function pairwisecomparenaive(xgpu, dgpu, J, Nx)
+    index = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+    stride = gridDim().x * blockDim().x
 
+    for i = index:stride:Nx
+        y = 0;
 
-x = rand(Nx, Nj)
-d = zeros(Nx, Nj)
+        for j = 1:Nx
+            y += F(xgpu[i, J] - xgpu[j, J]);
+        end
+
+        dgpu[i, J] = y / (Nx - 1);
+    end
+
+end
 
 # simple RELU
 function F(x)
@@ -18,16 +26,42 @@ function F(x)
     end
 end
 
-# weights
-w = Float64.(rand(1:5, Nj))
-w ./= sum(w)   
+function speedtestnaive(Nx, Nj)
+    x = rand(Nx, Nj)
+    d = zeros(Nx, Nj)
 
-xgpu = CuArray(x)
-dgpu = CuArray(d)
+    # weights
+    w = Float64.(rand(1:5, Nj))
+    w ./= sum(w)   
 
-function pairwisecompare(xgpu, dgpu, index)
-    i = (blockIdx().x - 1) * blockDim().x + threadIdx().x
-    stridex = gridDim().x * blockDim().x
+    xgpu = CuArray(x)
+    dgpu = CuArray(d)
+
+    index = 1
+
+    kernel = @cuda launch=false pairwisecomparenaive(xgpu, dgpu, index, Nx)
+
+    config = launch_configuration(kernel.fun)
+    println(config)
+    for crit = 1:Nj
+        CUDA.@sync begin
+            kernel(xgpu, dgpu, crit, Nx;
+            threads=config.threads, blocks=config.blocks)
+        end
+    end
+
+    d = Array(dgpu);
+
+    return d
 
 end
 
+
+
+# Oregon is about 254806 km², so .5km² cells gives
+Nx = 4 * (100000) # number of alternatives
+Nj = 30 # number of criteria
+
+@time begin
+    Φ⁺ = speedtestnaive(Nx,Nj)
+end
